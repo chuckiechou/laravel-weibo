8d61aeff0ad488a078f29615710b0524f4f4142d
chuckiechou <zzp208@qq.com>
2019-08-28 00:01:18 +0800

    用户注册页面

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,9 +1,16 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container ">
-        <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="{{route('help')}}">帮助</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-        </ul>
+@extends('layouts.default')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
     </div>
-</nav>
+@stop
